2ed0f951b2ff8f2b3c102ac24059ada9ab5e0eb5
Jeanette Mikkelsen <jeanette.mikkelsen@utb.ecutbildning.se>
2024-10-29 13:45:57 +0100

    lagt till responsivitet för InfoSection

--- src/Components/SectionInfo.jsx
+++ src/Components/SectionInfo.jsx
@@ -1,5 +1,12 @@
 import React from 'react';
-import Button from './Button'; // Justerar importen av knappkomponenten
+import Button from './Button';
+import InfoBox from './InfoBox'; 
+import creditCardIcon from '../Images/credit-card-icon.svg';
+import walletIcon from '../Images/wallet-icon.svg';
+import checkCircleIcon from '../Images/icons/check-circle.svg';
+import arrowRightIcon from '../Images/icons/arrowright.svg';
+import transferMoneyImage from '../Images/transfermoney.svg';
+import receivePaymentsImage from '../Images/receive-payments.svg';
 import './SectionInfo.css';
 
 const SectionInfo = () => {
@@ -10,48 +17,46 @@ const SectionInfo = () => {
                     <h2>Make your money transfer simple and clear</h2>
                     <ul className="infolist">
                         <li>
-                            <img src="/Images/icons/check-circle.svg" alt="check icon" className="checkicon" />
+                            <img src={checkCircleIcon} alt="check icon" className="checkicon" />
                             <p>Banking transactions are free for you</p>
                         </li>
                         <li>
-                            <img src="/Images/icons/check-circle.svg" alt="check icon" className="checkicon" />
+                            <img src={checkCircleIcon} alt="check icon" className="checkicon" />
                             <p>No monthly cash commission</p>
                         </li>
                         <li>
-                            <img src="/Images/icons/check-circle.svg" alt="check icon" className="checkicon" />
+                            <img src={checkCircleIcon} alt="check icon" className="checkicon" />
                             <p>Manage payments and transactions online</p>
                         </li>
                     </ul>
                     <Button aria-label="Learn more">
                         <span>Learn more</span>
-                        <img src="/Images/icons/arrowright.svg" alt="Arrow right icon" className="arrowicon" />
+                        <img src={arrowRightIcon} alt="Arrow right icon" className="arrowicon" />
                     </Button>
                 </div>
                 <div className="image1">
-                    <img src="/Images/transfermoney.svg" alt="A example of how you can transfer money in our App" className="inforight" />
+                    <img src={transferMoneyImage} alt="Example of transferring money in our app" className="inforight" />
                 </div>
                 <div className="image2">
-                    <img src="/Images/receive-payments.svg" alt="A example of how you can receive payments in our App" className="inforight" />
+                    <img src={receivePaymentsImage} alt="Example of receiving payments in our app" className="inforight" />
                 </div>
                 <div className="content2">
                     <h2>Receive payment from international bank details</h2>
                     <div className="info">
-                        <div className="info-box">
-                            <div className="infoicon">
-                                <img src="/Images/credit-card-icon.svg" alt="credit card icon" />
-                            </div>
-                            <p>Manage your payments online. Mollis congue egestas egestas fermentum fames.</p>
-                        </div>
-                        <div className="info-box">
-                            <div className="infoicon">
-                                <img src="/Images/wallet-icon.svg" alt="wallet icon" />
-                            </div>
-                            <p>A elementum and imperdiet enim, pretium etiam facilisi aenean quam mauris.</p>
-                        </div>
+                        <InfoBox
+                            icon={creditCardIcon}
+                            title=""
+                            description="Manage your payments online. Mollis congue egestas egestas fermentum fames."
+                        />
+                        <InfoBox
+                            icon={walletIcon}
+                            title=""
+                            description="A elementum and imperdiet enim, pretium etiam facilisi aenean quam mauris."
+                        />
                     </div>
                     <Button aria-label="Learn more">
                         <span>Learn more</span>
-                        <img src="/Images/icons/arrowright.svg" alt="Arrow right icon" className="arrowicon" />
+                        <img src={arrowRightIcon} alt="Arrow right icon" className="arrowicon" />
                     </Button>
                 </div>
             </div>
@@ -60,3 +65,5 @@ const SectionInfo = () => {
 };
 
 export default SectionInfo;
+
+
